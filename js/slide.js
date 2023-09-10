069d9acc09deee6b558b9f461babe7a871686a3a
Raphael <raphael.01@hotmail.com>
2023-09-10 09:58:13 -0300

    calculo de distancia e movimmetando as imagens

--- js/slide.js
+++ js/slide.js
@@ -1,36 +1,50 @@
 export default class Slide {
-constructor(slide, wrapper){
-  this.slide = document.querySelector(slide);
-  this.wrapper = document.querySelector(wrapper);
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, moviment: 0 }
+  }
 
+moveSlide(distX){
+  this.dist.movePosition = distX
+  this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
 }
 
-onStart(event){
-  event.preventDefault();
-  this.wrapper.addEventListener('mousemove', this.onMove)
+updatePosition(clientX){
+  this.dist.moviment = (this.dist.startX - clientX) *1.7;
+  return this.dist.finalPosition - this.dist.moviment;
 }
 
-onMove(event){
-}
+  onStart(event) {
+    event.preventDefault();
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener('mousemove', this.onMove)
+  }
 
-onEnd(){
-  this.wrapper.removeEventListener('mousemove', this.onMove)
-}
+  onMove(event) {
+   const finalPosition = this.updatePosition(event.clientX)
+   this.moveSlide(finalPosition);
+  }
 
-addSlideEvents(){
-  this.wrapper.addEventListener('mousedown', this.onStart)
-  this.wrapper.addEventListener('mouseup', this.onEnd)
-}
+  onEnd() {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition
+  }
 
-bindEvents(){
-  this.onStart = this.onStart.bind(this);
-  this.onMove = this.onMove.bind(this);
-  this.onEnd = this.onEnd.bind(this);
-}
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('mouseup', this.onEnd)
+  }
 
-init(){
-  this.bindEvents();
-  this.addSlideEvents();
-  return this;
-}
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
 }
